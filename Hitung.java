931b86a8497cac6f214ee1012fc6a8d9cc99c948
noobmas <limbuspopo@gmail.com>
2025-02-28 21:32:26 +0700

    Update Hitung.java

--- Hitung.java
+++ Hitung.java
@@ -1,28 +1,79 @@
 public class Hitung {
+
     public static void main(String[] args) {
         double sisi = 4;
         double alas = 3, tinggi = 4, sisi1 = 3, sisi2 = 4, sisi3 = 5;
         double jariJari = 5;
         double sisiKubus = 3;
 
-        double luasPersegi = sisi * sisi;
-        double kelilingPersegi = 4 * sisi;
-        System.out.println("Luas Persegi: " + luasPersegi);
-        System.out.println("Keliling Persegi: " + kelilingPersegi);
+        // Persegi
+        double luasPersegi = hitungLuasPersegi(sisi);
+        double kelilingPersegi = hitungKelilingPersegi(sisi);
+        printHasil("Persegi", luasPersegi, kelilingPersegi);
 
-        double luasSegitiga = 0.5 * alas * tinggi;
-        double kelilingSegitiga = sisi1 + sisi2 + sisi3;
-        System.out.println("Luas Segitiga: " + luasSegitiga);
-        System.out.println("Keliling Segitiga: " + kelilingSegitiga);
+        // Segitiga
+        double luasSegitiga = hitungLuasSegitiga(alas, tinggi);
+        double kelilingSegitiga = hitungKelilingSegitiga(sisi1, sisi2, sisi3);
+        printHasil("Segitiga", luasSegitiga, kelilingSegitiga);
 
-        double luasLingkaran = Math.PI * jariJari * jariJari;
-        double kelilingLingkaran = 2 * Math.PI * jariJari;
-        System.out.println("Luas Lingkaran: " + luasLingkaran);
-        System.out.println("Keliling Lingkaran: " + kelilingLingkaran);
+        // Lingkaran
+        double luasLingkaran = hitungLuasLingkaran(jariJari);
+        double kelilingLingkaran = hitungKelilingLingkaran(jariJari);
+        printHasil("Lingkaran", luasLingkaran, kelilingLingkaran);
 
-        double luasPermukaanKubus = 6 * sisiKubus * sisiKubus;
-        double volumeKubus = sisiKubus * sisiKubus * sisiKubus;
-        System.out.println("Luas Permukaan Kubus: " + luasPermukaanKubus);
-        System.out.println("Volume Kubus: " + volumeKubus);
+        // Kubus
+        double luasPermukaanKubus = hitungLuasPermukaanKubus(sisiKubus);
+        double volumeKubus = hitungVolumeKubus(sisiKubus);
+        printHasilKubus("Kubus", luasPermukaanKubus, volumeKubus);
+    }
+
+    // Metode untuk menghitung luas dan keliling persegi
+    public static double hitungLuasPersegi(double sisi) {
+        return sisi * sisi;
+    }
+
+    public static double hitungKelilingPersegi(double sisi) {
+        return 4 * sisi;
+    }
+
+    // Metode untuk menghitung luas dan keliling segitiga
+    public static double hitungLuasSegitiga(double alas, double tinggi) {
+        return 0.5 * alas * tinggi;
+    }
+
+    public static double hitungKelilingSegitiga(double sisi1, double sisi2, double sisi3) {
+        return sisi1 + sisi2 + sisi3;
+    }
+
+    // Metode untuk menghitung luas dan keliling lingkaran
+    public static double hitungLuasLingkaran(double jariJari) {
+        return Math.PI * jariJari * jariJari;
+    }
+
+    public static double hitungKelilingLingkaran(double jariJari) {
+        return 2 * Math.PI * jariJari;
+    }
+
+    // Metode untuk menghitung luas permukaan dan volume kubus
+    public static double hitungLuasPermukaanKubus(double sisi) {
+        return 6 * sisi * sisi;
+    }
+
+    public static double hitungVolumeKubus(double sisi) {
+        return sisi * sisi * sisi;
+    }
+
+    // Metode untuk mencetak hasil persegi, segitiga, dan lingkaran
+    public static void printHasil(String bentuk, double luas, double keliling) {
+        System.out.printf("%s:\n", bentuk);
+        System.out.printf("  Luas: %.2f\n", luas);
+        System.out.printf("  Keliling: %.2f\n\n", keliling);
+    }
+
+    // Metode untuk mencetak hasil kubus
+    public static void printHasilKubus(String bentuk, double luasPermukaan, double volume) {
+        System.out.printf("%s:\n", bentuk);
+        System.out.printf("  Luas Permukaan: %.2f\n", luasPermukaan);
+        System.out.printf("  Volume: %.2f\n", volume);
     }
 }
